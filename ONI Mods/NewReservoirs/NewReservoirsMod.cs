b21d418a2cb3d2ebcfdfb27d4873a3731efcf2f1
Bigal22 <Alesha0362@gmail.com>
2022-07-24 19:44:33 -0400

    New Reservoirs
    The plan is too make more advanced reservoirs that have a
    - larger storage (that is configurable),
    - maybe more input/output (maybe filtered),
    - (maybe more)....

---
+++ ONI Mods/NewReservoirs/NewReservoirsMod.cs
@@ -0,0 +1,15 @@
+using HarmonyLib;
+using KMod;
+using System.Collections.Generic;
+
+
+namespace NewReservoirs
+{
+    public class NewReservoirsMod : UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+        }
+    }
+}
---
+++ ONI Mods/NewReservoirs/NewReservoirsPatches.cs
@@ -0,0 +1,28 @@
+using HarmonyLib;
+using UnityEngine;
+
+namespace NewReservoirs
+{
+    public class NewReservoirsPatches
+    {
+        [HarmonyPatch(typeof(Db))]
+        [HarmonyPatch("Initialize")]
+        public class Db_Initialize_Patch
+        {
+
+            /*
+			 * 
+			 */
+
+            public static void Prefix()
+            {
+                Debug.Log("I execute before Db.Initialize!");
+            }
+
+            public static void Postfix()
+            {
+                Debug.Log("I execute after Db.Initialize!");
+            }
+        }
+    }
+}
