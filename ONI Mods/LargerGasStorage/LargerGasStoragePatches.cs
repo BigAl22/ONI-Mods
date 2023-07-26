8224ef7e63a5a056666088c153b822b51ab9f5e4
BigAl22 <Alesha0362@gmail.com>
2023-07-26 01:21:59 -0400

    LargerGasStorage V1.1.2 + Gas storage is now configurable ++ \AppData\Local\BDR\ONI\LargerGasStorage

--- ONI Mods/LargerGasStorage/LargerGasStoragePatches.cs
+++ ONI Mods/LargerGasStorage/LargerGasStoragePatches.cs
@@ -1,4 +1,7 @@
 using HarmonyLib;
+using KMod;
+using System;
+using System.IO;
 using UnityEngine;
 
 namespace LargerGasStorage
@@ -10,18 +13,63 @@ namespace LargerGasStorage
         public class GasReservoirConfig_ConfigureBuildingTemplate_Patch
         {
 
+
             /*
              * By default the gas reservoir stores 150kg, while the liquid reservoir stores 5000kg
              * 
              */
 
+            
+
+
+            #region Prefix used to check for the file
+            public static void Prefix(ref GameObject go)
+            {
+                Debug.Log("LargerGasStorage - Prefix: Checking if the file exists");
+
+                Debug.Log("LargerGasStorage - " + System.IO.Directory.GetCurrentDirectory());
+
+
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Local\\BDR\\ONI\\LargerGasStorage";
+                Debug.Log("LargerGasStorage - " + path);
+
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+                
+                path += "\\Values.txt";
+
+                if (!File.Exists(path)) {
+                    using (StreamWriter sw = (File.CreateText(path)))
+                    {
+                        sw.WriteLine("Change the value below to what you desire (in kg, 1 metric ton = 1000kg).");
+                        sw.WriteLine("5000");
+                    }
+                }
+
+            }
+            #endregion
+
+
+
             public static void Postfix(ref GameObject go)
-            { 
+            {
+                Debug.Log("LargerGasStorage - Postfix: Changing the value");
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Local\\BDR\\ONI\\LargerGasStorage\\Values.txt";
+
+                StreamReader sr = new StreamReader(path);
+                string[] lines = new string[2];
+                for (int i = 0; i < 2; i++){
+                    lines[i] = sr.ReadLine();
+                }
+
+                float value = float.Parse(lines[1]);
+                Debug.Log("LargerGasStorage - New value is : " + value);
+
                 #region The value displayed in the UI
 
                 go.AddOrGet<Reservoir>();
                 Storage defaultStorage = BuildingTemplates.CreateDefaultStorage(go);
-                defaultStorage.capacityKg = 5000f;
+                defaultStorage.capacityKg = value;
 
                 #endregion
 
@@ -29,7 +77,7 @@ namespace LargerGasStorage
                 #region The value used in the game Logic
 
                 var conduitConsumer = go.AddOrGet<ConduitConsumer>();
-                conduitConsumer.capacityKG = 5000f; 
+                conduitConsumer.capacityKG = value; 
 
                 #endregion
             }
